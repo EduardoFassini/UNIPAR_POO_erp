51b56696c161097051b153e1605a8f89a5ca391a
EduardoFassini <e.fassini@edu.unipar.br>
2024-05-28 22:02:22 -0300

    Initial commit

---
+++ src/main/java/br/unipar/erp/data/entity/Usuario.java
@@ -0,0 +1,34 @@
+package br.unipar.erp.data.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name="usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    private Integer codigo;
+
+    @Column(name="username")
+    private String usuario;
+
+    @Column(name="password")
+    private String senha;
+
+    private String nome;
+
+    @Column(name="nascimento")
+    private LocalDate dataNascimento;
+
+}
